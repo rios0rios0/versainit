ad56ff59a90c1cff86b586c49caa54c845ac534e
k4yt3x <i@k4yt3x.com>
2023-05-25 17:13:43 +0000

    ran gofmt for the previously changed main.go

--- main.go
+++ main.go
@@ -167,15 +167,15 @@ func processProject(globalConfig *GlobalConfig, config *Config) error {
 	if err != nil {
 		return err
 	}
-    
-    logger.Info("Getting CHANGELOG.md's path relative to repository root")
-    changelogRelativePath, err := filepath.Rel(changelogPath, projectPath)
+
+	logger.Info("Getting CHANGELOG.md's path relative to repository root")
+	changelogRelativePath, err := filepath.Rel(changelogPath, projectPath)
 	if err != nil {
 		return err
 	}
 
 	logger.Info("Adding version file to the worktree")
-    result, err := w.Add(changelogRelativePath)
+	result, err := w.Add(changelogRelativePath)
 	if err != nil {
 		logger.Errorf("Result not expected: %v", result)
 		return err
